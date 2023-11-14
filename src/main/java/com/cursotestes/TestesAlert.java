e4313e63b637dc90b1e0931e4bde4e6972bad288
Maísa Moreira <maisamoreira@Maisas-MacBook-Air.local>
2023-11-14 17:33:55 -0300

    teste alert com confirm

--- src/main/java/com/cursotestes/TestesAlert.java
+++ src/main/java/com/cursotestes/TestesAlert.java
@@ -17,10 +17,35 @@ public class TestesAlert {
         // Conseguir mudar o foco para o alert e interagir com ele
         Alert alert = driver.switchTo().alert();
         String texto = alert.getText();
-        Assert.assertEquals("Alert Simples", alert.getText());
+        Assert.assertEquals("Alert Simples", texto);
         alert.accept();
 
         driver.findElement(By.id("elementosForm:nome")).sendKeys(texto);
         driver.quit();
     }
+
+    @Test
+    public void testarConfirmSimples() {
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().setSize(new Dimension(1024, 768));
+        driver.manage().window().setPosition(new Point(1800, 20));
+        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+
+        driver.findElement(By.id("confirm")).click();
+        Alert alert = driver.switchTo().alert();
+        Assert.assertEquals("Confirm Simples", alert.getText());
+        alert.accept();
+        Assert.assertEquals("Confirmado", alert.getText());
+        alert.accept();
+
+        driver.findElement(By.id("confirm")).click();
+        alert = driver.switchTo().alert();
+        Assert.assertEquals("Confirm Simples", alert.getText());
+        alert.dismiss();
+        Assert.assertEquals("Negado", alert.getText());
+        alert.dismiss();
+
+        driver.quit();
+    }
+
 }
