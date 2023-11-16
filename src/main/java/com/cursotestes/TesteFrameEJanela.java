33146f83dc4f42b6a0f6850ca65e46e5621e65c8
Maísa Moreira <maisammoreira@gmail.com>
2023-11-16 14:51:50 -0300

    teste janela

--- src/main/java/com/cursotestes/TesteFrameEJanela.java
+++ src/main/java/com/cursotestes/TesteFrameEJanela.java
@@ -7,9 +7,7 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-import java.awt.*;
-
-public class TesteFrame {
+public class TesteFrameEJanela {
 
     @Test
     public void testarFrames() {
@@ -28,6 +26,26 @@ public class TesteFrame {
         driver.switchTo().defaultContent();
         driver.findElement(By.id("elementosForm:nome")).click();
         driver.findElement(By.id("elementosForm:nome")).sendKeys(msg);
+
+        driver.quit();
+    }
+
+    @Test
+    public void testarJanela() {
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().setSize(new Dimension(1024, 768));
+        driver.manage().window().setPosition(new Point(1800, 20));
+        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+
+        String janelaOriginal = driver.getWindowHandle();
+
+        driver.findElement(By.id("buttonPopUpEasy")).click();
+        driver.switchTo().window("Popup");
+        driver.findElement(By.tagName("textarea")).sendKeys("Deu certo?");
+        driver.close();
+
+        driver.switchTo().window(janelaOriginal);
+        driver.findElement(By.tagName("textarea")).sendKeys("Oi, mundo");
 
         driver.quit();
     }
